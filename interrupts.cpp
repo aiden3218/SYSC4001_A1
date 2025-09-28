3e6e9042e4b3cce819add512330b1c2b7fb7dee3
aiden3218 <aident.3218@gmail.com>
2025-09-28 02:48:08 -0400

    need to figure out how to add to execution file

--- interrupts.cpp
+++ interrupts.cpp
@@ -19,9 +19,12 @@ int main(int argc, char** argv) {
     std::string execution;  //!< string to accumulate the execution output
 
     /******************ADD YOUR VARIABLES HERE*************************/
-
-
-
+    std::string CPU = "CPU";
+    std::string SYSCALL = "SYSCALL";
+    std::string ENDIO = "END_IO";
+    int total_time = 0;
+    int time = 0;
+    FILE *executionFile = fopen("executionFile.txt", "w");
     /******************************************************************/
 
     //parse each line of the input trace file
@@ -30,11 +33,32 @@ int main(int argc, char** argv) {
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
 
+        if (activity == CPU) {
+            fprintf(executionFile, "%i, %i, CPU execution\n", total_time, duration_intr);
+            //execution = ("%i, %i, CPU execution\n", total_time, time);
+            total_time += duration_intr;
+        }
+        else if (activity == SYSCALL) {
+            fprintf(executionFile, "%i, %i, switch to kernel mode\n", total_time, 1);
+            //execution.append("%i, %i, switch to kernel mode\n", total_time, 1);
+            total_time += 1;
+            fprintf(executionFile, "%i, %i, context saved\n", total_time, 10);
+            //execution.append("%i, %i, context saved\n", total_time, 10);
+            total_time += 10;
+            fprintf(executionFile, "%i, %i, find vector %i in memory position %c\n", total_time, 1, duration_intr, vectors[duration_intr]);
+            //execution.append("%i, %i, find vector %i in memory position %i", total_time, 1, duration_intr, vectors[duration_intr]);
+            total_time += 1;
+        }
+        else {
+
+        }
 
 
         /************************************************************************/
 
     }
+
+    fclose(executionFile);
 
     input_file.close();
 
